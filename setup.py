7f1adab4c7c4c11c81f503ebe5f81da770f35e58
Hobson Lane <laneh@sharplabs.com>
2013-10-08 14:29:34 -0700

    self_requirements for use in other projects

--- setup.py
+++ setup.py
@@ -34,11 +34,11 @@ def parse_dependency_links(file_name):
 setup(
     name='hl-django-nvd3',
     version=django_nvd3.__version__,
-    description="Hobson Lane's Django NVD3 Fork -- Template tags for hl-python-nvd3 package",
+    description="Hobson Lane's Django NVD3 -- Django template tags (hooks) for python-nvd3 -- python bindings for nvd3.js which wraps d3.j3 into reusable charts",
     long_description=read('README.rst'),
     keywords='django, nvd3, chart, graph, d3',
     url='http://github.com/hobsonlane/hl-django-nvd3',
-    author='Hobson Lane and Belaid Arezqui',
+    author='Belaid Arezqui, portions by Hobson Lane',
     author_email='hobsonlane@gmail.com',
     license='MIT License',
     zip_safe=False,
