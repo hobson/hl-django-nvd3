18b566277fb0990f2ac714976f755aa3a7415647
Hobson Lane <laneh@sharplabs.com>
2013-10-08 15:19:41 -0700

    use pip in setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,35 @@
 from setuptools import setup, find_packages
 import django_nvd3
 import os
-import re
+#import re
+from pip.req import parse_requirements, parse_dependency_links
 
 
 def read(*parts):
     return open(os.path.join(os.path.dirname(__file__), *parts)).read()
 
 
-def parse_requirements(file_name):
-    requirements = []
-    for line in open(file_name, 'r').read().split('\n'):
-        if re.match(r'(\s*#)|(\s*$)', line):
-            continue
-        if re.match(r'\s*-e\s+', line):
-            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
-        elif re.match(r'(\s*git)|(\s*hg)', line):
-            pass
-        else:
-            requirements.append(line)
-    return requirements
+# def parse_requirements(file_name):
+#     requirements = []
+#     for line in open(file_name, 'r').read().split('\n'):
+#         if re.match(r'(\s*#)|(\s*$)', line):
+#             continue
+#         if re.match(r'\s*-e\s+', line):
+#             requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
+#         elif re.match(r'(\s*git)|(\s*hg)', line):
+#             pass
+#         else:
+#             requirements.append(line)
+#     return requirements
 
 
-def parse_dependency_links(file_name):
-    dependency_links = []
-    for line in open(file_name, 'r').read().split('\n'):
-        if re.match(r'\s*-[ef]\s+', line):
-            dependency_links.append(re.sub(r'\s*-[ef]\s+', '', line))
+# def parse_dependency_links(file_name):
+#     dependency_links = []
+#     for line in open(file_name, 'r').read().split('\n'):
+#         if re.match(r'\s*-[ef]\s+', line):
+#             dependency_links.append(re.sub(r'\s*-[ef]\s+', '', line))
 
-    return dependency_links
+#     return dependency_links
 
 
 setup(
@@ -45,8 +46,8 @@ setup(
     packages=find_packages(exclude=["tests", "demoproject", "docs"]),
     include_package_data=True,
     package_data={},
-    install_requires=parse_requirements('requirements.txt'),
-    dependency_links=parse_dependency_links('requirements.txt'),
+    install_requires=[str(line.req) for line in parse_requirements('requirements.txt')],
+    # dependency_links=parse_dependency_links('requirements.txt'),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
